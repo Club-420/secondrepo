a90a5cd7f8686307a163fe437ae9bac288358705
ShresthaPrajwal <57761143+ShresthaPrajwal@users.noreply.github.com>
2021-08-17 23:34:57 +0545

    Dev2 (#1)
    * feri change gare
    
    * V3
    
    Co-authored-by: ImPrajwalAcharya <prajwalacp222@gmail.com>

--- cd.cpp
+++ cd.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
-
+int multiply(int,int);
 int main(){
-    int a,b,c;
+    int a=2,b=2,c;
     c=multiply(a,b);
     cout<<c;
     return 0;
+}
+int multiply(int x,int y){
+    return x*y;
 }
