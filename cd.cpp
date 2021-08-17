def8031059f61f4e749b9afb7eec9edce1c5d2e1
ImPrajwalAcharya <prajwalacp222@gmail.com>
2021-08-18 00:07:43 +0545

    ayo feri ayo

--- cd.cpp
+++ cd.cpp
@@ -11,13 +11,17 @@ int add(int x,int y){
 int sub(int x, int y){
     return x-y;
 }
+int dividegar(int x, int y){
+    return x/y;
+}
 
 int main()
 {
-    int a = 2, b = 2, c,d,e;
+    int a = 2, b = 2, c,d,e,h;
     c = multiply(a, b);
     d=add(a,b);
     e=sub(a,b);
+    h=dividegar(a,b);
     cout << c;
     return 0;
 }
