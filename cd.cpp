2412ce96850fc4898fdca61cb886e36c351b1fe3
ShresthaPrajwal <pjkoju@gmail.com>
2021-08-18 00:04:30 +0545

    v6

--- cd.cpp
+++ cd.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 
 using namespace std;
-int multiply(int, int);
-
-int main()
-{
-    int a = 2, b = 2, c,d,e;
-    c = multiply(a, b);
-    d=add(a,b);
-    e=sub(a,b);
-    cout << c;
-    return 0;
-}
-
 int multiply(int x, int y)
 {
     return x * y;
@@ -22,4 +10,14 @@ int add(int x,int y){
 }
 int sub(int x, int y){
     return x-y;
+}
+
+int main()
+{
+    int a = 2, b = 2, c,d,e;
+    c = multiply(a, b);
+    d=add(a,b);
+    e=sub(a,b);
+    cout << c;
+    return 0;
 }
