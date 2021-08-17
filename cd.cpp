064bf015d0221ba7ab55e2644867dd92603f6a20
ImPrajwalAcharya <prajwalacp222@gmail.com>
2021-08-18 00:20:11 +0545

    ayo feri ayo feri

--- cd.cpp
+++ cd.cpp
@@ -14,15 +14,17 @@ int sub(int x, int y){
 float division(int x,int y){
     return x/y;
     }
+int dividegar(int x, int y){
 int main()
 {
-    int a = 2, b = 2, c,d,e;
+    int a = 2, b = 2, c,d,e,f,g;
     float div;
     c = multiply(a, b);
     d=add(a,b);
     e=sub(a,b);
     div=division(a,b);
-int dividegar(int x, int y){
-    return x/y;
+    f=incre(a);
+    g=incre(b);
+    return 0;
 }
 
