7ee80fc980d27b8ccaa591bb3efa1fa41d3c83d7
ShresthaPrajwal <pjkoju@gmail.com>
2021-08-17 23:48:08 +0545

    v4

--- cd.cpp
+++ cd.cpp
@@ -1,12 +1,17 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-int multiply(int,int);
-int main(){
-    int a=2,b=2,c;
-    c=multiply(a,b);
-    cout<<c;
+int multiply(int, int);
+
+int main()
+{
+    int a = 2, b = 2, c;
+    c = multiply(a, b);
+    cout << c;
     return 0;
 }
-int multiply(int x,int y){
-    return x*y;
+
+int multiply(int x, int y)
+{
+    return x * y;
 }
